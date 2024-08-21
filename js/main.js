bbe222f220ac94fce3d8337e908404ec4a44f8b1
Al-Amin Sarker <alaminsarker5657@gmail.com>
2024-08-22 01:08:59 +0600

    jquery practice

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    const swiper = new Swiper('.swiper', {
+        effect: "cube",
+        grabCursor: true,
+        cubeEffect: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        speed: 1000,
+
+        direction: 'horizontal',
+        loop: true,
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction'
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: true,
+        },
+
+
+    });
+
+})
